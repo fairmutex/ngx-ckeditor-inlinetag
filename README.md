8f3b81812ca281c2228983dad04cd140554b049e
fairmutex <stephan.galea@gmail.com>
2019-03-25 02:33:02 +0100

    3 files modified, 2 added, 1 deleted

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # ngx-ckeditor-inlinetag
 
 [Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ngx-ckeditor-inlinetag)
+
+
+ckeditor 4 plugin for configurable inline tags.
+
+* Icon dont load on StackBlitz
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { SanitizeHtmlPipe } from './sanitizeHtml.pipe';
 
 import { AppComponent } from './app.component';
 import { CKEditorModule } from 'ng2-ckeditor';
 
 @NgModule({
   imports:      [ BrowserModule,CKEditorModule, FormsModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, SanitizeHtmlPipe ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
